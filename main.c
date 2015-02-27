d809243ebc1f15007999abd8fe5aa588102b4a68
verove_j <gottin_o@epitech.eu>
2015-02-27 10:14:32 +0100

    je suis un fdp

--- main.c
+++ main.c
@@ -5,15 +5,34 @@
 ** Login   <gazzol_j@epitech.net>
 ** 
 ** Started on  Mon Feb 23 09:36:12 2015 julien gazzola
-** Last update Thu Feb 26 17:36:30 2015 Jordan Verove
+** Last update Thu Feb 26 19:01:44 2015 Jordan Verove
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "Philo.h"
 
-int		total_rice = 280;
+int		total_rice = 350;
 pthread_mutex_t	*mutex_tab;
+
+void		print_philo_state(t_philo *philo)
+{
+  static int	i = 0;
+  
+  if (philo->left == 0 && philo->right == 0)
+    printf("Philo : %d - state : sleep - rice : %d\n", philo->id, philo->rice);
+  else if (philo->left == 1 && philo->right == 1)
+    printf("Philo : %d - state : eat - rice : %d\n", philo->id, philo->rice);
+  else
+    printf("Philo : %d - state : think - rice : %d\n", philo->id, philo->rice);
+  if (i == 6)
+    {
+      printf("\n");
+      i = 0;
+    }
+  else
+    i = i + 1;
+}
 
 void	*take_cs(t_philo *philo)
 {
@@ -21,7 +40,6 @@ void	*take_cs(t_philo *philo)
     {
       if (pthread_mutex_trylock(&mutex_tab[philo->id % 7]) == 0)
 	philo->left = 1;
-      sleep(1);
       if (pthread_mutex_trylock(&mutex_tab[philo->id - 1]) == 0)
 	philo->right = 1;
     }
@@ -36,10 +54,11 @@ void	*take_cs(t_philo *philo)
 
 void			unlock_mutex_tab(t_philo *philo)
 {
-  if (pthread_mutex_unlock(&mutex_tab[philo->id % 7]) == 0)
+  if (pthread_mutex_unlock(&mutex_tab[philo->id]) == 0)
     philo->left = 0;
   if (pthread_mutex_unlock(&mutex_tab[philo->id - 1]) == 0)
     philo->right = 0;
+  printf("UNLOCK philo[%d]--------------------------------------\n", philo->id);
 }
 
 void			eat(t_philo *philo)
@@ -50,11 +69,11 @@ void			eat(t_philo *philo)
     {
       philo->state = TIRED;
       philo->rice -= 10;
-      pthread_mutex_lock(&mutex);
+      // pthread_mutex_lock(&mutex);
       total_rice -= 10;
-      printf("philo rice[%d] : %d\ntotal rice :%d\n", philo->id, philo->rice, total_rice);
-      //      unlock_mutex_tab(philo);
-      pthread_mutex_unlock(&mutex);
+      //      printf("philo rice[%d] : %d\ntotal rice :%d\n", philo->id, philo->rice, total_rice);
+      //unlock_mutex_tab(philo);
+      // pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -65,10 +84,11 @@ void			*philosopher(void *arg)
   philo = arg;
   while (total_rice != 0)
     {
-      if (philo->state == HUNGRY)
+      if (philo->left == 0 && philo->right == 0)
 	take_cs(philo);
       philo->state == HUNGRY;
       eat(philo);
+      print_philo_state(philo);
       // printf("PHILO[%d]: __ left : %c ------- right : %c\n", philo->id, philo->left, philo->right);
       sleep(1);
       unlock_mutex_tab(philo);
---
+++ philo.c
@@ -0,0 +1,10 @@
+/*
+** philo.c for philo in /home/verove_j/rendu/PSU_2014_philo
+** 
+** Made by Jordan Verove
+** Login   <verove_j@epitech.net>
+** 
+** Started on  Mon Feb 23 13:03:07 2015 Jordan Verove
+** Last update Mon Feb 23 13:44:16 2015 Jordan Verove
+*/
+
